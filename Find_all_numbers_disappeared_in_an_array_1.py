8f2345b6ad0fb96305c59aaba5fa454e6fb3f8fc
Vedika Gurav <gurav.vedika22@gmail.com>
2026-05-26 13:14:16 +0530

    Add files via upload

---
+++ Find_all_numbers_disappeared_in_an_array_1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+
+        arr = [(nums[i], i) for i in range(len(nums))]
+        arr.sort()
+
+        ans = [0] * len(nums)
+
+        for i in range(len(arr)):
+
+            if i > 0 and arr[i][0] == arr[i-1][0]:
+                ans[arr[i][1]] = ans[arr[i-1][1]]
+
+            else:
+                ans[arr[i][1]] = i
+
+        return ans
+#Find_all_numbers_disappeared_in_an_array.
---
+++ Find_all_numbers_disappeared_in_an_array_2.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        return [i for i in range(1, len(nums)+1) if i not in s]
+        
